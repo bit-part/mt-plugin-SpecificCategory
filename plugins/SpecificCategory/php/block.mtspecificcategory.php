87b7c3aec6135aa3afdc45be2bfaf18ae26ee1e3
tinybeans <register@tinybeans.net>
2015-01-23 19:04:53 +0900

    [improve] Improved the label modifier (Dynamic)
    You can set categories separated by slash.

--- plugins/SpecificCategory/php/block.mtspecificcategory.php
+++ plugins/SpecificCategory/php/block.mtspecificcategory.php
@@ -19,8 +19,9 @@ function smarty_block_mtspecificcategory($args, $content, &$ctx, &$repeat) {
         }
         else {
             $terms['blog_id'] = ($args['blog_id']) ? $args['blog_id'] : $blog_id;
+            $terms['show_empty'] = 1;
             if ($args['label']) {
-                $terms['label'] = $args['label'];
+                $terms['label'] = explode('/', $args['label']);
             }
             if ($args['basename']) {
                 $terms['basename'] = $args['basename'];
@@ -31,11 +32,30 @@ function smarty_block_mtspecificcategory($args, $content, &$ctx, &$repeat) {
             else {
                 $cat = $ctx->mt->db()->fetch_categories($terms);
             }
-            if (isset($cat)) {
-                $cat = $cat[0];
-            }
         }
         if (isset($cat)) {
+            if ($args['label'] && count($terms['label']) > 1) {
+                $parent_id;
+                for ($i = 0; $i < count($terms['label']); $i++) {
+                    foreach ($cat as $obj) {
+                        if ($terms['label'][$i] != $obj->label) {
+                            continue;
+                        }
+                        if ($i == 0) {
+                            $parent_id = $obj->id;
+                        }
+                        elseif ($parent_id == $obj->parent) {
+                            $parent_id = $obj->id;
+                            if ($i == (count($terms['label']) - 1)) {
+                                $cat = $obj;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (is_array($cat)) {
+                $cat = $cat[0];
+            }
             $ctx->localize($localvars);
             $ctx->stash('conditional', 1);
             $ctx->stash('category', $cat);
