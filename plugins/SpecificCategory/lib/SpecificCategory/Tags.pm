8405a052ff26eac1f7eb7d78643331e703664cd4
tinybeans <register@tinybeans.net>
2015-01-23 16:05:53 +0900

    [improve] Improved the label modifier (Static)
    You can set categories separated by slash.

--- plugins/SpecificCategory/lib/SpecificCategory/Tags.pm
+++ plugins/SpecificCategory/lib/SpecificCategory/Tags.pm
@@ -13,21 +13,47 @@ sub hdlr_specificcategory {
     my $class = $blog->{column_values}{class};
     my $class_type = $args->{class_type} || 'category';
 
-    my (%terms, %args);
+    my (%terms, %args, @labels);
 
     if ($args->{id}) {
         $terms{id} = $args->{id};
-    } elsif (!defined $args->{blog_id} and $class eq 'website') {
+    }
+    elsif (!defined $args->{blog_id} and $class eq 'website') {
         return $ctx->error(MT->translate('MTSpecific[_1] must be used with blog_id modifier in a website context.', ucfirst($class_type)));
-    } else {
+    }
+    else {
         $terms{blog_id} = ($args->{blog_id}) ? $args->{blog_id} : $blog_id;
-        $terms{label} = $args->{label} if $args->{label};
         $terms{basename} = $args->{basename} if $args->{basename};
+        if ($args->{label}) {
+            @labels = split(/\//, $args->{label});
+            $terms{label} = \@labels;
+        }
     }
 
     my $class = MT->model($class_type);
-    my $cat = $class->load(\%terms, {limit => 1})
+    my @cats = $class->load(\%terms)
         or return MT::Template::Context::_hdlr_pass_tokens_else(@_);
+    my $cat;
+
+    if ($args->{label} and $#labels > 0) {
+        my $parent_id;
+        for (my $i = 0; $i <= $#labels; $i++) {
+            foreach my $obj (@cats) {
+                next if ($labels[$i] ne $obj->label);
+                if ($i == 0) {
+                    $parent_id = $obj->id;
+                }
+                elsif ($parent_id == $obj->parent) {
+                    $parent_id = $obj->id;
+                    $cat = $obj if $i == $#labels;
+                }
+            }
+        }
+    }
+    else {
+        $cat = @cats[0];
+    }
+    return '' unless $cat;
 
     my $res = '';
     my $tokens = $ctx->stash('tokens');
